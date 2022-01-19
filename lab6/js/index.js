997bee2db113ce05d14926e5b83c9d7dd1e9e07f
Miguel Tornero <a00820449@itesm.mx>
2022-01-19 17:49:19 -0600

    added functionality

---
+++ lab6/js/index.js
@@ -0,0 +1,29 @@
+function start() {
+    //Adds item adding functionality
+    $(".agregar").click(function(e){
+        e.preventDefault();
+        
+        const todoText = $("#newText").val();
+        
+        const todoElem = $(`<li class="lis"></li>`)
+        .append($(`<div class="itemShop"></div>`).text(todoText))
+        .append($(`<button class="checar">check</button>`))
+        .append($(`<button class="del">delete</button>`));
+        
+        console.log(todoElem)
+        
+        $(".Lista").append(todoElem);
+    });
+
+    //Adds item checking functionality
+    $(".Lista").on("click", ".checar", function(){
+        $(this).parent().children(".itemShop").toggleClass("chec");
+    });
+
+    //Adds item deleting functionality
+    $(".Lista").on("click", ".del", function(){
+        $(this).parent().remove();
+    });
+}
+
+$(start)
