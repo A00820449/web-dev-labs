b2a200ffbe4ade36a3b33ab3ddee67be7327661e
Miguel Tornero <a00820449@itesm.mx>
2022-01-20 09:20:54 -0600

    adding todo now clears textbox

--- lab5/js/script.js
+++ lab5/js/script.js
@@ -1,71 +1,41 @@
-//1 Obtener las referencias de los elementos a interactuar
- 
-let post = document.getElementById("ButtonPost")
-let clear = document.getElementById("ButtonClear")
-let mark = document.getElementById("ButtonMark")
-let del = document.getElementById("ButtonDelete")
- 
-// Registrar el Evento
- 
-post.addEventListener("click",TodoPost)
-clear.addEventListener("click",TodoClear)
-mark.addEventListener("click",TodoMark)
-del.addEventListener("click",TodoDel)
+
+function start(){
+    $("#ButtonPost").on("click",TodoPost)
+    $("#ButtonClear").on("click",TodoClear)
+    $("#ButtonMark").on("click",TodoMark)
+    $("#ButtonDelete").on("click",TodoDel)
+}
  
 function TodoPost(e) {
     e.preventDefault()
+
+    let todoText = $("#todoText").val().trim();
+
+    if (todoText === "") {return;}
+
+    $("#todoText").val("");
     
-    let todo = document.getElementById("todoText").value
-    let list = document.getElementById("todoList")
+    let div = $(`<div class="todo"></div>`)
+    .append(
+        $(`<label></label>`)
+        .append($(`<input type="checkbox" class="todoCheckBox"/>`))
+        .append($(`<span></span>`).text(todoText))
+    );
     
-    let div = document.createElement("div")
-    let input = document.createElement("input")
-    let label = document.createElement("label")
-    
-    input.type = "checkbox"
-    input.name = "todo"
-    
-    label.textContent = todo
-    
-    div.appendChild(input)
-    div.appendChild(label)
-    
-    list.append(div)
+    $("#todoList").append(div)
  
 }
  
 function TodoClear(){
- 
-    let todos = document.getElementsByName("todo")
-    for(const todo of todos){
-        todo.checked = false
-    }
- 
+    $(".todoCheckBox").prop("checked", false);
 }
  
 function TodoMark() {
- 
-    let todos = document.getElementsByName("todo")
-    for(const todo of todos){
-        todo.checked = true
-    }
- 
+    $(".todoCheckBox").prop("checked", true);
 }
  
 function TodoDel() {
- 
-    let todos = document.getElementsByName("todo")
-
-    let checkedList = [];
-
-    for (const todo of todos) {
-        if (todo.checked) {
-            checkedList.push(todo.parentElement)
-        }
-    }
-
-    for (const todo of checkedList) {
-        todo.remove()
-    }
- 
+    $("#todoList .todo input:checked").parent().parent().remove();
 }
+
+$(start)
