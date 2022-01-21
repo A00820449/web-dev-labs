0145173e1c49b418b5f8fbee8b76bf78f32bfce4
Miguel Tornero <a00820449@itesm.mx>
2022-01-20 19:22:42 -0600

    added ajax functionality

--- lab7/script.js
+++ lab7/script.js
@@ -1,7 +1,34 @@
+const animals = ["dog", "cat", "bear", "duck", "monkey", "lion", "tiger", "shark", "parrot", "penguin"];
+const API_KEY = "pgdoxa7pFTvlyOfYO0D6kY6K44c85Gsc"; // don't steal pls
+
+const buttonsDiv = $("#animal-buttons");
+const animalsDiv = $("#animals");
+
+function renderImages(res) {
+    console.log(res);
+    animalsDiv.html("");
+    for (const gifData of res.data) {
+        animalsDiv
+        .append($(`<img/>`).attr("src", gifData.images.original.url));
+    }
+}
+
 $(document).ready(function() {
+    // Start your code from here
+    for (const animal of animals) {
+        buttonsDiv.append($(`<button></button>`).text(animal));
+    }
 
-// Start your code from here
-
-
+    buttonsDiv.on("click", "button", function(){
+        const animal = $(this).text() || "";
+        //window.alert(animal);
+        $.ajax({
+            url: `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${animal}&limit=10`,
+            success: renderImages,
+            error(e) {
+                console.log(e);
+            }
+        });
+    });
 
 });
