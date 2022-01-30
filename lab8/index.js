8791110a0c06233a1fe687f25f283f7a17d2df01
Miguel Tornero <a00820449@itesm.mx>
2022-01-29 21:29:08 -0600

    added functionality to tables page

--- lab8/index.js
+++ lab8/index.js
@@ -4,12 +4,40 @@ const app = express();
 
 const PORT = parseInt(process.env.PORT) || 3000;
 
+/**
+ * @typedef {{name: string, phone: string, email: string}} Table
+ */
+
+/**
+ * @type {Table[]}
+ */
+let reservedTables = [{name: "Bob", phone: "555-1234-567", email: "bob@test.com"}];
+
+/**
+ * @type {Table[]}
+ */
+let waitlistTables = [];
+
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.redirect("/home.html");
 });
 
+app.get("/api/tables", (req,res)=>{
+    res.json(reservedTables);
+});
+
+app.get("/api/waitlist", (req,res)=>{
+    res.json(waitlistTables);
+});
+
+app.get("/api/clear", (req,res)=>{
+    reservedTables = [];
+    waitlistTables = [];
+    res.json({response: "Cleared all tables"})
+})
+
 app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port:`, PORT);
 });
